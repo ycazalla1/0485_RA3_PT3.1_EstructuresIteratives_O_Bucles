03dd603c78786aed1ec6c7defb5155d5486e68d4
ycaza <ycaza@DESKTOP-T1UA6VD>
2025-04-10 20:54:28 +0200

    Correccó classe Act2DoWhile

--- src/main/java/com/mycompany/estructuresrepetitivesobucles/Act2DoWhile.java
+++ src/main/java/com/mycompany/estructuresrepetitivesobucles/Act2DoWhile.java
@@ -5,33 +5,46 @@
 package com.mycompany.estructuresrepetitivesobucles;
 import java.util.Scanner;
 /**
- *
+ * Classe Act2DoWhile.
+ * Imprimeix varis cops la frase a escriure en la pissarra.
  * @author Bagner i Yamila
  */
 public class Act2DoWhile {
     
     public static void main(String[] args) {
         
+        //Declarem una variable int, que serà el valor del número introduit.
         int numero;
-        int j = 0;
+        /* Declarem una variable int, que farà de comprovant de número gran i
+        instanciador. */
         int i = 1;
+        //Declarem un final int per tenir un mínim de vegades obligatori.
+        final int MINIM_VEGADES = 500;
+        //eclarem un final int per tenir una frase constant.
+        final String FRASE_COPIAR = ". Estaré atent a classe.";
         
-        Scanner lector = new Scanner(System.in);
+        Scanner lector = new Scanner(System.in); /*Declarem una varieble de
+        tipus Scanner perquè l'usuari puguin introduir un valor.*/
         
+        //Missatge per preguntar per un número.
         System.out.print("Introdueix un número gran: ");
-        numero = lector.nextInt();
+        numero = lector.nextInt(); //Ordre per introduir un número.
         
-        while (j == 0) {
-            if (numero >= 10){
+        //DoWhile per preguntar de nou si no és un número gran l'introduit.
+        do {
+            //If que comprova si és gran el número.
+            if (numero >= MINIM_VEGADES){
+                //DoWhile per imprimir el número de vegades la frase.
                 do {
-                    System.out.println(i + ". Estaré atent classe.");
-                    i = i + 1;
-                    j = j + 1;
+                    //Missatge que mostra el número de la frase i la frase.
+                    System.out.println(i + FRASE_COPIAR);
+                    i = i + 1; //Incrementador.
                 } while (i <= numero);
-            } else {
+            } else { /* Si no és un número gran, mostra un missatge per tornar-lo
+                     a escriure. */
                 System.out.println("És un número massa petit. Torna'l a escriure. -> ");
-                numero = lector.nextInt();
+                numero = lector.nextInt(); //Ordre per introduir un número.
             }
-        }
+        } while (i == 1);
     }
 }
