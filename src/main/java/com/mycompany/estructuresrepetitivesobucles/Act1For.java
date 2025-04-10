2384ab910a357e4cae575a6c2988fe2be5c4f149
ycaza <ycaza@DESKTOP-T1UA6VD>
2025-04-10 09:21:57 +0200

    Correció classe Act1For

--- src/main/java/com/mycompany/estructuresrepetitivesobucles/Act1For.java
+++ src/main/java/com/mycompany/estructuresrepetitivesobucles/Act1For.java
@@ -5,35 +5,49 @@
 package com.mycompany.estructuresrepetitivesobucles;
 import java.util.Scanner;
 /**
- *
+ * Classe Act1For.
+ * Calcula la potència utilitzant el bucle for.
  * @author Bagner i Yamila
  */
 public class Act1For {
     
     public static void main(String[] args) {
         
+        //Declarem una variable int, que serà el valor del número introduit.
         int numero;
+        //Declarem una variable int, que serà per obtenir la potencia.
         int potencia;
         
-        Scanner lector = new Scanner(System.in);
+        Scanner lector = new Scanner(System.in); /*Declarem una varieble de
+        tipus Scanner perquè l'usuari puguin introduir un valor.*/
         
+        //Missatge per preguntar per un número.
         System.out.print("Introdueix un nombre: ");
-        numero = lector.nextInt();
+        numero = lector.nextInt(); //Ordre per introduir un número.
         
+        //Missatge per preguntar per la potència a la que es vol elevar.
         System.out.print("A quina potència vols calcular? ");
-        potencia = lector.nextInt();
+        potencia = lector.nextInt(); //Ordre per introduir una potència.
         
+        //Declarem una variable int per guardar el numero en el resultat.
         int resultat = numero;
         
+        //If que comprova si el valor introduit no és negatiu.
         if (potencia > 0){
-            
+            //For per calcular la potència
             for (int i = 1; i < potencia; i++) {
+                //Es va incrementant el resultat multiplicant-se pel número.
                 resultat *= numero;
             }
-            System.out.println("La potència de " + numero + " a " + potencia + " és igual a: " + resultat);
             
-        } else {
-            System.out.println("La potència introduïda és negativa. No es pot realitzar el càlcul.");
+            //Missatge que mostra el resultat de la potència.
+            System.out.println("La potència de " + numero + " elevat a "
+                    + potencia + " és igual a: " + resultat);
+            
+        } else { //Si el nombre és negatiu executa el següent.
+            //Missatge per indicar que el l'entrada no és válida.
+            System.out.println("La potència introduïda és negativa. No es pot"
+                    + " realitzar el càlcul.");
         }
         
     }
